f8a94a3dbf3df0727856e02967c17f842d926b25
tycoon7 <timothy.coon@gmail.com>
2017-02-09 12:01:41 -0500

    Finished first submission of HW#2

--- Homework/HW2_MAE5803/HW2P2_plotPhasePortrait.m
+++ Homework/HW2_MAE5803/HW2P2_plotPhasePortrait.m
@@ -1,9 +1,10 @@
 %%
-function HW1P2_plotPhasePortrait(eqnum,tspan,xLimits,yLimits,plotTitle)
+function HW2P2_plotPhasePortrait(eqnum,tspan,xLimits,yLimits,plotTitle)
 % PLOTPHASEPORTRAIT plots the phase portrait
-figure(eqnum);
+set(0,'DefaultTextInterpreter','latex')
+figure();
 hold on
-for i = linspace(xLimits(1),xLimits(2),5)
+for i = linspace(xLimits(1),xLimits(2),7)
     for j = linspace(yLimits(1),yLimits(2),5)
         X0 = [i; j];
         [t,X] = ode45(@stateEqn,tspan,X0,[],eqnum);
@@ -12,7 +13,7 @@ for i = linspace(xLimits(1),xLimits(2),5)
         plot(X0(1),X0(2),'+','color',c);
     end
 end
-axis([-2 2 -2 2])
+xlim(xLimits); ylim(yLimits);
 axis equal
 xlabel('$x$')
 ylabel('$\dot{x}$')
@@ -25,7 +26,7 @@ function [dx] = stateEqn(t, x, flag)
 %P1stateEqn contains state eqn for MAE5803 HW1 P1
 % flag: choose which equation to integrate
 %
-dx = zeros(2,1);
+dx = zeros(size(x));
 if flag == 1
     dx(1) = -x(1)^3 + sin(x(1))^4;
     dx(2) = 0;
@@ -36,7 +37,7 @@ elseif flag == 2
     
 elseif flag == 3   
     dx(1) = x(2);
-    dx(2) = x(1)^2*sin(x(1))^8*cos(3*x(1))^2 - x(1)^7 - x(2);
+    dx(2) = x(1)^2*sin(x(1))^8*cos(3*x(1))^2 - x(1)^7 - x(2)^5;
     
 elseif flag == 4
     dx(1) = x(2);
